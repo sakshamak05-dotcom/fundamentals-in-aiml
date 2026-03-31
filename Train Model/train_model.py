51591d1203a367269de2212206b539c47c5a9eff
sakshamak05-dotcom <sakshamak05@gmail.com>
2026-03-31 22:56:07 +0530

    Update train_model.py

--- Train Model/train_model.py
+++ Train Model/train_model.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pickle
 
+data = pd.read_csv("data/student_data.csv")
+
+X = data.drop("final_result", axis=1)
+y = data["final_result"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+pickle.dump(model, open("model/model.pkl", "wb"))
